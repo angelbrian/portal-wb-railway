0a35c409e368c740de7f166adbceb2682dd10890
angello <abrr.dev@gmail.com>
2024-10-24 14:38:00 -0600

    fix

--- helpers/commons.js
+++ helpers/commons.js
@@ -1,2 +1,4 @@
 
-export const allCompanies = [  'MVS', 'VFJ', 'COR', 'PAT', 'DNO', 'MOV', 'DOS', 'VEC', 'ACT', 'GDL', 'OCC', 'REN', 'FYJ', 'GAR', 'RUT', 'MIN', 'HMS', 'DAC', 'AGS', 'SIN', 'RPL' ];
+module.exports = {
+    allCompanies: [ 'MVS', 'VFJ', 'COR', 'PAT', 'DNO', 'MOV', 'DOS', 'VEC', 'ACT', 'GDL', 'OCC', 'REN', 'FYJ', 'GAR', 'RUT', 'MIN', 'HMS', 'DAC', 'AGS', 'SIN', 'RPL' ]
+};
